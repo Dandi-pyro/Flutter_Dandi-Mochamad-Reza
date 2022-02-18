7f6541a8182647ca43220f6f713328f22a08ca99
Dandi-pyro <81307704+Dandi-pyro@users.noreply.github.com>
2022-02-18 16:41:36 +0700

    Section 3

---
+++ 3_Introduction Mobile App Development/praktikum/Praktikum.dart
@@ -0,0 +1,16 @@
+/*
+!. 	- C++ untuk Windows
+	- Python untuk Development Android
+	- Java untuk OOP
+	- Go untuk Development Android
+	- Swift untuk IOS
+
+2. Yang ada di IOS tidak ada di Android
+	- Infinity Blade
+	- GarageBand
+	- AppStore
+   Yang ada di Android tidak ada di IOS
+	- DroidLock
+	- ES File Explorer
+	- Google Play Store
+  */
