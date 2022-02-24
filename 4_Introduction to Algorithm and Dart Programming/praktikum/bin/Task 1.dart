bc2d08daad40a19e5d3e1b1e5664d8728287a60f
Dandi M. Reza <dandimr16@gmail.com>
2022-02-24 14:59:39 +0700

    commit 'Section 4'

---
+++ 4_Introduction to Algorithm and Dart Programming/praktikum/bin/Task 1.dart
@@ -0,0 +1,9 @@
+void main(List<String> arguments) {
+  double jari_jari = 10;
+  double Luas = Lingkaran(jari_jari);
+  print(Luas);
+}
+
+double Lingkaran(double jari_jari){
+  return 3.14*jari_jari*jari_jari;
+}
