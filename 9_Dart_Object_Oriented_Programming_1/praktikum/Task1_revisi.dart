a177fe829da39ced7d4346c9f27d9c9c226ce45c
Dandi-pyro <81307704+Dandi-pyro@users.noreply.github.com>
2022-03-09 23:08:32 +0700

    Section 9

---
+++ 9_Dart_Object_Oriented_Programming_1/praktikum/Task1_revisi.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+class Hewan {
+  var nama;
+  late int berat;
+}
+
+class Mobil {
+  late int kapasitas = 0;
+  List<Hewan> muatan = [];
+  void tambahMuatan(int berat, Hewan h1){
+    if (kapasitas-berat>=0){
+      muatan.add(h1);
+      kapasitas -= berat;
+      print("Berhasil ditambahkan");
+    } else {
+      print("Kapasitas tidak mencukupi, sisa kapasitas : $kapasitas");
+    }
+  }
+}
+
+void main() {
+  var tampung = Mobil();
+  var h1 = Hewan();
+  stdout.write("Silahkan masukkan kapasitas muatan : ");
+  String? strInput = stdin.readLineSync();
+  if (strInput!=null){
+    tampung.kapasitas = int.tryParse(strInput)!;
+    while (tampung.kapasitas>0){
+      stdout.write("masukkan nama hewan : ");
+      h1.nama = stdin.readLineSync();
+      stdout.write("masukkan berat hewan : ");
+      String? strInput1 = stdin.readLineSync();
+      if (strInput1!=null){
+        h1.berat = int.tryParse(strInput1)!;
+        tampung.tambahMuatan(h1.berat, h1);
+      }
+    }
+  }
+}
